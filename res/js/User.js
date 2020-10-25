bbd6b08c1538891c81c78ad2085cefe500138f15
Kristo Markov <markovkristo@gmail.com>
2020-10-25 14:27:47 +0200

    Ül1

---
+++ res/js/User.js
@@ -0,0 +1,7 @@
+class User {
+    constructor(firstname, lastname, email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+}
---
+++ res/js/script.js
@@ -0,0 +1,46 @@
+$(function () {
+    $(".avatar").click(function () {
+        var X = $(this).attr('id');
+        if (X == 1) {
+            $(".dropdown-content").hide();
+            $(this).attr('id', '0');
+        } else {
+            $(".dropdown-content").show();
+            $(this).attr('id', '1');
+            loadUserInfo()
+                .then(function (response) {
+                    user = new User(
+                        response.firstname,
+                        response.lastname,
+                        response.email
+                    );
+                    displayUserInfo(user)
+                })
+                .catch(function () {
+                    alert('Error loading user info')
+                });
+        }
+    })
+
+})
+
+function displayUserInfo(user) {
+    $('#name').text(user.firstname + " " + user.lastname);
+    $('#mail').text(user.email);
+}
+
+function loadUserInfo() {
+    return $.get(
+        {
+            url: 'https://private-anon-90de795816-wad20postit.apiary-mock.com/users/1',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    );
+}
+
+/**/
